be7abada239bca3fe5f7f52b859d9403639782fb
dvoraka <alen.dvorak@gmail.com>
2017-09-24 17:36:14 +0200

    Prepare libraries and configuration

--- src/main/java/dvoraka/pricechecker/App.java
+++ src/main/java/dvoraka/pricechecker/App.java
@@ -1,13 +1,28 @@
 package dvoraka.pricechecker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class);
-        System.out.println("App");
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("App");
+        };
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.build();
     }
 }
