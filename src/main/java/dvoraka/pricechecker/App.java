db09b57ed5895a353238995e0952f605669da814
dvoraka <alen.dvorak@gmail.com>
2018-01-21 17:35:38 +0100

    Testing possibilities

--- src/main/java/dvoraka/pricechecker/App.java
+++ src/main/java/dvoraka/pricechecker/App.java
@@ -1,10 +1,13 @@
 package dvoraka.pricechecker;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -15,9 +18,15 @@ public class App {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner() {
+    public CommandLineRunner commandLineRunner(RestTemplate restTemplate) {
         return args -> {
-            System.out.println("App");
+            ResponseEntity<String> result = restTemplate
+                    .getForEntity("https://m.alza.cz/intel-core-i7-7820x-d4977502.htm", String.class);
+//            System.out.println(result.getHeaders());
+//            System.out.println(result.getBody());
+
+            Document doc = Jsoup.parse(result.getBody());
+            System.out.println(doc.select("div.price").first().select("div.normal").first().text());
         };
     }
 
